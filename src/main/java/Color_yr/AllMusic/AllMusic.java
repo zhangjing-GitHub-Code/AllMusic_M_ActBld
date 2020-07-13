0c6723a925b670c619c31df5c592dd2aced8e4ef
Color_yr <402067010@qq.com>
2020-07-13 20:13:56 +0800

    fix on URL

--- src/main/java/Color_yr/AllMusic/AllMusic.java
+++ src/main/java/Color_yr/AllMusic/AllMusic.java
@@ -10,6 +10,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.Identifier;
 
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class AllMusic implements ModInitializer {
@@ -53,6 +54,23 @@ public class AllMusic implements ModInitializer {
         HudShow.save = null;
     }
 
+    public static URL Get(URL url) {
+        if (url.toString().contains("http://music.163.com/song/media/outer/url?id=")) {
+            try {
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(4 * 1000);
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 Chrome/42.0.2311.90 Safari/537.36");
+                connection.connect();
+                connection.getResponseCode();
+                return connection.getURL();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return url;
+    }
+
     public static void onClicentPacket(final String message) {
         final Thread asyncThread = new Thread(() -> {
             try {
@@ -63,6 +81,9 @@ public class AllMusic implements ModInitializer {
                     MinecraftClient.getInstance().getSoundManager().stopSounds(null, SoundCategory.RECORDS);
                     stopPlaying();
                     nowURL = new URL(message.replace("[Play]", ""));
+                    nowURL = Get(nowURL);
+                    if (nowURL == null)
+                        return;
                     nowPlaying.SetMusic(nowURL.openStream());
                     nowPlaying.play();
                 } else if (message.startsWith("[Lyric]")) {
