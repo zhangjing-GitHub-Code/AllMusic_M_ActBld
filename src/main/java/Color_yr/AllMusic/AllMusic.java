412bdeba8f90f7c6fbef35dccf3432d175dfd7af
Color_yr <402067010@qq.com>
2021-06-24 22:29:21 +0800

    up

--- src/main/java/Color_yr/AllMusic/AllMusic.java
+++ src/main/java/Color_yr/AllMusic/AllMusic.java
@@ -14,27 +14,9 @@ import java.nio.charset.StandardCharsets;
 
 public class AllMusic implements ModInitializer {
     public static final Identifier ID = new Identifier("allmusic", "channel");
-    private static final APlayer nowPlaying = new APlayer();
+    private static APlayer nowPlaying;
     public static boolean isPlay = false;
-    public static int v = -1;
     private static URL nowURL;
-
-    public final Thread thread = new Thread(() -> {
-        while (true) {
-            try {
-                if (MinecraftClient.getInstance().options != null) {
-                    int nowV = (int) (MinecraftClient.getInstance().options.getSoundVolume(SoundCategory.RECORDS) *
-                            MinecraftClient.getInstance().options.getSoundVolume(SoundCategory.MASTER) * 100);
-                    if (v != nowV) {
-                        nowPlaying.Set(nowV);
-                    }
-                }
-                Thread.sleep(100);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    });
 
     public static void onServerQuit() {
         try {
@@ -124,6 +106,6 @@ public class AllMusic implements ModInitializer {
                 e.printStackTrace();
             }
         });
-        thread.start();
+        nowPlaying = new APlayer();
     }
 }
