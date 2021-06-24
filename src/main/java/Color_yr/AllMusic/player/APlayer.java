6981ee5144a840e9308736d4e75239424e52db34
Color_yr <402067010@qq.com>
2021-06-24 20:53:53 +0800

    up

--- src/main/java/Color_yr/AllMusic/player/APlayer.java
+++ src/main/java/Color_yr/AllMusic/player/APlayer.java
@@ -1,23 +1,3 @@
-/*
- * 11/19/04		1.0 moved to LGPL.
- * 29/01/00		Initial version. mdm@techie.com
- *-----------------------------------------------------------------------
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License as published
- *   by the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Library General Public License for more details.
- *
- *   You should have received a copy of the GNU Library General Public
- *   License along with this program; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *----------------------------------------------------------------------
- */
-
 package Color_yr.AllMusic.player;
 
 import Color_yr.AllMusic.player.decoder.BuffPack;
@@ -25,33 +5,45 @@ import Color_yr.AllMusic.player.decoder.IDecoder;
 import Color_yr.AllMusic.player.decoder.flac.DataFormatException;
 import Color_yr.AllMusic.player.decoder.flac.FlacDecoder;
 import Color_yr.AllMusic.player.decoder.mp3.Mp3Decoder;
+import io.netty.util.internal.ThreadLocalRandom;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.*;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.openal.AL10;
+import org.lwjgl.system.MemoryUtil;
 
-import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.AudioFormat;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
 
-/**
- * The <code>Player</code> class implements a simple player for playback
- * of an MPEG audio stream.
- *
- * @author Mat McGowan
- * @since 0.0.8
- */
-
-// REVIEW: the audio device should not be opened until the
-// first MPEG audio frame has been decoded. 
 public class APlayer {
 
     private HttpClient client;
-    private SoundAudioDevice audio;
     private IDecoder decoder;
     private boolean isClose;
+    private ChannelManager sndSystem;
+    private ChannelManager.Entry channelmanager;
+    private AudioFormat audioformat;
+    private int index;
+    private boolean init;
 
     public APlayer() {
         try {
+            SoundHandler handler = Minecraft.getInstance().getSoundHandler();
+            SoundEngine soundManager = ObfuscationReflectionHelper.getPrivateValue(SoundHandler.class, handler, "field_147694_f");
+            sndSystem = ObfuscationReflectionHelper.getPrivateValue(SoundEngine.class, soundManager, "field_217941_k");
             client = HttpClientBuilder.create().useSystemProperties().build();
-            audio = new SoundAudioDevice();
             isClose = true;
         } catch (Exception e) {
             e.printStackTrace();
@@ -67,60 +59,65 @@ public class APlayer {
                 decoder = new Mp3Decoder();
                 decoder.set(client, url);
             }
-            audio.open(decoder);
+            audioformat = new AudioFormat(decoder.getOutputFrequency(),
+                    16,
+                    decoder.getOutputChannels(),
+                    true,
+                    false);
+            if (channelmanager == null) {
+                channelmanager = sndSystem.createChannel(SoundSystem.Mode.STREAMING);
+                channelmanager.runOnSoundExecutor((ex) -> {
+                    index = ObfuscationReflectionHelper.getPrivateValue(SoundSource.class, ex, "field_216441_b");
+                    init = true;
+                });
+            }
+            while (!init) ;
             isClose = false;
         }
     }
 
-    public void Set(int a) {
-        if (audio == null)
-            return;
-        FloatControl temp = audio.getVolctrl();
-        if (temp != null) {
-            float temp1 = (a == 0) ? -80.0f : ((float) (a * 0.2 - 35.0));
-            temp.setValue(temp1);
-        }
-    }
-
     public void play() throws Exception {
-        boolean ret = true;
-        while (ret) {
-            ret = decodeFrame();
+        while (true) {
+            try {
+                if (isClose)
+                    break;
+
+                BuffPack output = decoder.decodeFrame();
+                if (output == null)
+                    break;
+
+                int[] aint = new int[1];
+                AL10.alGenBuffers(aint);
+                int buff = aint[0];
+                ByteBuffer buffer = ByteBuffer.wrap(output.buff, 0, output.len);
+                AL10.alBufferData(buff, AL10.AL_FORMAT_STEREO16,buffer , (int) audioformat.getSampleRate());
+                AL10.alSourceQueueBuffers(index, buff);
+                int m_numprocessed = AL10.alGetSourcei(index, AL10.AL_BUFFERS_PROCESSED);
+                int m_numqueued = AL10.alGetSourcei(index, AL10.AL_BUFFERS_QUEUED);
+                int stateVaue = AL10.alGetSourcei(index, AL10.AL_SOURCE_STATE);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                break;
+            }
         }
-        synchronized (this) {
+        AL10.alSourcePlay(index);
+        while (true) {
+            if (isClose)
+                break;
+            Thread.sleep(10);
+        }
+        if (!isClose)
             close();
-        }
     }
 
     public void close() throws Exception {
         isClose = true;
+        if (channelmanager != null) {
+            channelmanager.runOnSoundExecutor(SoundSource::func_216418_f);
+            channelmanager.release();
+        }
         if (decoder != null)
             decoder.close();
-        if (audio != null)
-            audio.close();
-    }
-
-    protected boolean decodeFrame() {
-        try {
-            if (audio == null)
-                return false;
-            if (isClose)
-                return false;
-
-            BuffPack output = decoder.decodeFrame();
-            if (output == null)
-                return false;
-
-            synchronized (this) {
-                if (audio != null && !isClose) {
-                    audio.write(output.buff, output.len);
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
     }
 }
