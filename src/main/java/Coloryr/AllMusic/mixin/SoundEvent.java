a08c846c1135d6ff72ba96565513b3fc6d17d13e
Color_yr <402067010@qq.com>
2021-08-29 11:50:33 +0800

    up

---
+++ src/main/java/Coloryr/AllMusic/mixin/SoundEvent.java
@@ -0,0 +1,25 @@
+package Coloryr.AllMusic.mixin;
+
+import Coloryr.AllMusic.AllMusic;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.client.sound.SoundSystem;
+import net.minecraft.sound.SoundCategory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SoundSystem.class)
+public class SoundEvent {
+    @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
+    public void Quit(SoundInstance soundInstance, CallbackInfo info) {
+        if (AllMusic.isPlay) {
+            SoundCategory data = soundInstance.getCategory();
+            switch (data) {
+                case RECORDS:
+                case MUSIC:
+                    info.cancel();
+            }
+        }
+    }
+}
