5304b84e7496eaa10f050ebc849411a6ead42820
Color_yr <402067010@qq.com>
2022-10-15 19:07:13 +0800

    up

--- src/main/java/Coloryr/AllMusic/AllMusic.java
+++ src/main/java/Coloryr/AllMusic/AllMusic.java
@@ -17,13 +17,12 @@ import java.nio.charset.StandardCharsets;
 public class AllMusic implements ModInitializer {
     public static final Identifier ID = new Identifier("allmusic", "channel");
     public static APlayer nowPlaying;
-    public static boolean isPlay = false;
+    public static String url;
     public static HudUtils hudUtils;
 
     public static void onServerQuit() {
         try {
-            nowPlaying.close();
-            hudUtils.close();
+            stopPlaying();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -33,43 +32,38 @@ public class AllMusic implements ModInitializer {
     }
 
     public static void onClicentPacket(final String message) {
-        new Thread(() -> {
-            try {
-                if (message.equals("[Stop]")) {
-                    stopPlaying();
-                } else if (message.startsWith("[Play]")) {
-                    MinecraftClient.getInstance().getSoundManager().stopSounds(null, SoundCategory.MUSIC);
-                    MinecraftClient.getInstance().getSoundManager().stopSounds(null, SoundCategory.RECORDS);
-                    stopPlaying();
-                    nowPlaying.setMusic(message.replace("[Play]", ""));
-                } else if (message.startsWith("[Lyric]")) {
-                    hudUtils.Lyric = message.substring(7);
-                } else if (message.startsWith("[Info]")) {
-                    hudUtils.Info = message.substring(6);
-                } else if (message.startsWith("[List]")) {
-                    hudUtils.List = message.substring(6);
-                } else if (message.startsWith("[Img]")) {
-                    hudUtils.setImg(message.substring(5));
-                } else if (message.startsWith("[Pos]")) {
-                    nowPlaying.set(message.substring(5));
-                } else if (message.equalsIgnoreCase("[clear]")) {
-                    hudUtils.close();
-                } else if (message.startsWith("{")) {
-                    hudUtils.setPos(message);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }, "allmusic").start();
-    }
-
-    private static void stopPlaying() {
         try {
-            nowPlaying.close();
-            hudUtils.close();
+            if (message.equals("[Stop]")) {
+                stopPlaying();
+            } else if (message.startsWith("[Play]")) {
+                MinecraftClient.getInstance().getSoundManager().stopSounds(null, SoundCategory.MUSIC);
+                MinecraftClient.getInstance().getSoundManager().stopSounds(null, SoundCategory.RECORDS);
+                stopPlaying();
+                url = message.replace("[Play]", "");
+                nowPlaying.setMusic(url);
+            } else if (message.startsWith("[Lyric]")) {
+                hudUtils.Lyric = message.substring(7);
+            } else if (message.startsWith("[Info]")) {
+                hudUtils.Info = message.substring(6);
+            } else if (message.startsWith("[List]")) {
+                hudUtils.List = message.substring(6);
+            } else if (message.startsWith("[Img]")) {
+                hudUtils.setImg(message.substring(5));
+            } else if (message.startsWith("[Pos]")) {
+                nowPlaying.set(message.substring(5));
+            } else if (message.equalsIgnoreCase("[clear]")) {
+                hudUtils.close();
+            } else if (message.startsWith("{")) {
+                hudUtils.setPos(message);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void stopPlaying() {
+        nowPlaying.closePlayer();
+        hudUtils.close();
     }
 
     public static void drawText(String item, float x, float y){
@@ -119,6 +113,7 @@ public class AllMusic implements ModInitializer {
     public static void reload(){
 
     }
+
     @Override
     public void onInitialize() {
         ClientPlayNetworking.registerGlobalReceiver(ID, (client, handler, buffer, responseSender) -> {
