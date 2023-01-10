ab7f265175f486fc9761674d186c713dfc68d481
Color_yr <402067010@qq.com>
2023-01-10 21:57:07 +0800

    up

--- src/main/java/Coloryr/AllMusic/AllMusic.java
+++ src/main/java/Coloryr/AllMusic/AllMusic.java
@@ -6,9 +6,7 @@ import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.common.network.FMLNetworkEvent;
@@ -20,24 +18,25 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.SoundCategory;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiIngame;
-import net.minecraft.command.*;
-import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.sound.PlaySoundEvent17;
 import net.minecraftforge.common.MinecraftForge;
-import org.apache.logging.log4j.Logger;
 import org.lwjgl.opengl.GL11;
 
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-@Mod(modid = "allmusic", version = "2.6.3")
+@Mod(modid = "allmusic", version = "2.6.5")
 public class AllMusic {
     private static APlayer nowPlaying;
-    private HudUtils HudUtils;
+    private static HudUtils hudUtils;
     private String url;
+    private static int ang = 0;
+    private static int count = 0;
+
+    private static ScheduledExecutorService service;
 
     public static void sendMessage(String data) {
         FMLClientHandler.instance().getClient().func_152344_a(() -> {
@@ -50,7 +49,10 @@ public class AllMusic {
     @Mod.EventHandler
     public void test(final FMLLoadCompleteEvent event) {
         nowPlaying = new APlayer();
-        HudUtils = new HudUtils();
+        hudUtils = new HudUtils();
+
+        service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(AllMusic::time1, 0, 1, TimeUnit.MILLISECONDS);
     }
 
     @Mod.EventHandler
@@ -90,9 +92,9 @@ public class AllMusic {
         } catch (Exception e1) {
             e1.printStackTrace();
         }
-        HudUtils.Lyric = HudUtils.Info = HudUtils.List = "";
-        HudUtils.haveImg = false;
-        HudUtils.save = null;
+        hudUtils.Lyric = hudUtils.Info = hudUtils.List = "";
+        hudUtils.haveImg = false;
+        hudUtils.save = null;
     }
 
     @SubscribeEvent
@@ -112,20 +114,20 @@ public class AllMusic {
                 url = message.replace("[Play]", "");
                 nowPlaying.setMusic(url);
             } else if (message.startsWith("[Lyric]")) {
-                HudUtils.Lyric = message.substring(7);
+                hudUtils.Lyric = message.substring(7);
             } else if (message.startsWith("[Info]")) {
-                HudUtils.Info = message.substring(6);
+                hudUtils.Info = message.substring(6);
             } else if (message.startsWith("[Img]")) {
-                HudUtils.setImg(message.substring(5));
+                hudUtils.setImg(message.substring(5));
             } else if (message.startsWith("[Pos]")) {
                 nowPlaying.set(message.substring(5));
             } else if (message.startsWith("[List]")) {
-                HudUtils.List = message.substring(6);
+                hudUtils.List = message.substring(6);
             } else if (message.equalsIgnoreCase("[clear]")) {
-                HudUtils.Lyric = HudUtils.Info = HudUtils.List = "";
-                HudUtils.haveImg = false;
+                hudUtils.Lyric = hudUtils.Info = hudUtils.List = "";
+                hudUtils.haveImg = false;
             } else if (message.startsWith("{")) {
-                HudUtils.setPos(message);
+                hudUtils.setPos(message);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -136,7 +138,7 @@ public class AllMusic {
     @SideOnly(Side.CLIENT)
     public void onRenderOverlay(final RenderGameOverlayEvent.Post e) {
         if (e.type == RenderGameOverlayEvent.ElementType.PORTAL) {
-            HudUtils.update();
+            hudUtils.update();
         }
     }
 
@@ -152,22 +154,35 @@ public class AllMusic {
     }
 
     public static void drawPic(int textureID, int size, int x, int y) {
+        int a = size / 2;
+
         GL11.glBindTexture(3553,textureID);
         GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        GL11.glEnable(GL11.GL_ALPHA);
         GL11.glPushMatrix();
-        GL11.glTranslatef((float) x, (float) y, 0.0f);
+        GL11.glTranslatef((float) x + a, (float) y + a, 0.0f);
+
+        if(hudUtils.save.EnablePicRotate && hudUtils.thisRoute) {
+            GL11.glRotatef(ang, 0, 0, 1f);
+        }
+
+        int x0 = -a;
+        int x1 = a;
+        int y0 = -a;
+        int y1 = a;
         GL11.glBegin(7);
         GL11.glTexCoord2f(0.0f, 0.0f);
-        GL11.glVertex3f(0.0f, 0.0f, 0.0f);
+        GL11.glVertex3f(x0, y0, 0.0f);
         GL11.glTexCoord2f(0.0f, 1.0f);
-        GL11.glVertex3f(0.0f, (float) size, 0.0f);
+        GL11.glVertex3f(x0, y1, 0.0f);
         GL11.glTexCoord2f(1.0f, 1.0f);
-        GL11.glVertex3f((float) size, (float) size, 0.0f);
+        GL11.glVertex3f(x1, y1, 0.0f);
         GL11.glTexCoord2f(1.0f, 0.0f);
-        GL11.glVertex3f((float) size, 0.0f, 0.0f);
+        GL11.glVertex3f(x1, y0, 0.0f);
         GL11.glEnd();
         GL11.glPopMatrix();
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+        GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
     }
 
     public static void drawText(String item, float x, float y) {
@@ -176,9 +191,21 @@ public class AllMusic {
         gui.drawString(font, item, (int) x, (int) y, 0xffffff);
     }
 
+    private static void time1() {
+        if (hudUtils.save == null)
+            return;
+        if (count < hudUtils.save.PicRotateSpeed) {
+            count++;
+            return;
+        }
+        count = 0;
+        ang++;
+        ang = ang % 360;
+    }
+
     private void stopPlaying() {
         nowPlaying.closePlayer();
-        HudUtils.close();
+        hudUtils.close();
     }
 
     public static void runMain(Runnable runnable){
