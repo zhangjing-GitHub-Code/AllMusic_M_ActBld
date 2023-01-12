aa02e4bd52abd157df078c57639589b055a86fa3
Color_yr <402067010@qq.com>
2023-01-12 12:22:54 +0800

    up

--- src/main/java/coloryr/allmusic/mixin/QuitServer.java
+++ src/main/java/coloryr/allmusic/mixin/QuitServer.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class QuitServer {
-    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;)V", at = @At("TAIL"))
+    @Inject(method = "disconnect()V", at = @At("TAIL"))
     public void Quit(CallbackInfo info) {
         AllMusic.onServerQuit();
     }
